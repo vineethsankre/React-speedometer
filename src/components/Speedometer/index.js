6de42c0f0ee49b882ccc3a1dd3bf0a29de173ae7
vineethsankre <lsvineethsankre@gmail.com>
2024-04-03 18:11:36 +0000

    changed indentations

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -3,12 +3,19 @@ import './index.css'
 
 class Speedometer extends Component {
   state = {speed: 0}
+
   onAccelerate = () => {
-    this.setState(prevState => ({speed:prevState.speed + 10}))
+    if (this.state.speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
   }
+
   onBrake = () => {
-    this.setState(prevState => ({speed:prevState.speed - 10}))
+    if (this.state.speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
   }
+
   render() {
     const {speed} = this.state
     return (
@@ -23,10 +30,18 @@ class Speedometer extends Component {
           <h1 className="speed-text">Speed is {speed}mph</h1>
           <p className="speed-limit">Min Limit is 0mph, Max Limit is 200mph</p>
           <div className="buttons-container">
-            <button className="button accelerate-btn" type="button" onClick={this.onAccelerate}>
+            <button
+              className="button accelerate-btn"
+              type="button"
+              onClick={this.onAccelerate}
+            >
               Accelerate
             </button>
-            <button className="button brake-btn" type="button" onClick={this.onBrake}>
+            <button
+              className="button brake-btn"
+              type="button"
+              onClick={this.onBrake}
+            >
               Apply Brake
             </button>
           </div>
