f2ec72a957b4b9cc7c7131bb2edf424c19389699
vineethsankre <lsvineethsankre@gmail.com>
2024-04-03 17:44:11 +0000

    static webpage created

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import './index.css'
+
+class Speedometer extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="banner-container">
+          <h1 className="heading">SPEEDOMETER</h1>
+          <img
+            className="image"
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+          />
+          <p className="speed">Speed is 0mph</p>
+          <p className="speed-limit">
+            Min limit is 0mph and Max Limit is 200mph
+          </p>
+          <div className="buttons-container">
+            <button className="button accelerate-btn" type="button">
+              Accelerate
+            </button>
+            <button className="button brake-btn" type="button">
+              Apply Brake
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
