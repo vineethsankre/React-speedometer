99e51fc3be8bdbc25f1fb40a677e3d73dafd7b52
vineethsankre <lsvineethsankre@gmail.com>
2024-04-03 17:57:00 +0000

    Functionality added

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -2,7 +2,15 @@ import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
+  state = {speed: 0}
+  onAccelerate = () => {
+    this.setState(prevState => ({speed:prevState.speed + 10}))
+  }
+  onBrake = () => {
+    this.setState(prevState => ({speed:prevState.speed - 10}))
+  }
   render() {
+    const {speed} = this.state
     return (
       <div className="bg-container">
         <div className="banner-container">
@@ -12,15 +20,13 @@ class Speedometer extends Component {
             src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
             alt="speedometer"
           />
-          <h1 className="speed">Speed is 0mph</h1>
-          <p className="speed-limit">
-            Min Limit is 0mph, Max Limit is 200mph
-          </p>
+          <h1 className="speed-text">Speed is {speed}mph</h1>
+          <p className="speed-limit">Min Limit is 0mph, Max Limit is 200mph</p>
           <div className="buttons-container">
-            <button className="button accelerate-btn" type="button">
+            <button className="button accelerate-btn" type="button" onClick={this.onAccelerate}>
               Accelerate
             </button>
-            <button className="button brake-btn" type="button">
+            <button className="button brake-btn" type="button" onClick={this.onBrake}>
               Apply Brake
             </button>
           </div>
